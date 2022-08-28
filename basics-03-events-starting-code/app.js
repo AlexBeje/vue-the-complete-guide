eb13fa97228287b6d0144c2bf34e0077f8400c9f
AlexBeje <alex.bejenaru93@gmail.com>
2022-08-28 19:36:06 +0200

    📦 NEW: Added method arguments and event.target.value handle example.

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,14 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "Your name",
     };
   },
   methods: {
-    add() {
-      this.counter >= 0 && this.counter++;
+    setName(event, uppercase) {
+      this.name = uppercase === 'toUppercase'
+        ? event.target.value.toUpperCase()
+        : event.target.value;
     },
-    reduce() {
-      this.counter && this.counter--;
+    add(number = 1) {
+      this.counter = this.counter >= 0 && this.counter + number;
+    },
+    reduce(number = 1) {
+      this.counter =
+        this.counter && this.counter >= number
+          ? this.counter - number
+          : this.counter;
     },
   },
 });
