b0ff8c72953366ea1ed94a942824ef570ae5819d
AlexBeje <alex.bejenaru93@gmail.com>
2022-09-12 15:30:12 +0200

    📦 NEW: Added dynamic style example.

---
+++ basics-10-styling-starting-setup/app.js
@@ -0,0 +1,24 @@
+Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  methods: {
+    selectBox(box) {
+      switch (box) {
+        case 'A':
+          this.boxASelected = !this.boxASelected;
+          break;
+        case 'B':
+          this.boxBSelected = !this.boxBSelected;
+          break;
+        case 'C':
+          this.boxCSelected = !this.boxCSelected;
+          break;
+      }
+    }
+  }
+}).mount("#styling");
