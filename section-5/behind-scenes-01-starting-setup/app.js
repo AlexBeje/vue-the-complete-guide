0d7a0915d23f840c78f1d482870c55eb315929b6
AlexBeje <alex.bejenaru93@gmail.com>
2022-09-13 17:01:49 +0200

    📦 NEW: Added reactivity example (behind the scene)

---
+++ section-5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+// ...
+
+app.mount('#app');
+
+let message = 'Hello';
+let longMessage = message + 'World'
+
+// console.log('👩‍❤️‍👩', longMessage)
+
+message = 'Hello!!!!'
+
+// console.log('😪', longMessage)
+
+// ...
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello!World!'
+}
+
+const handler = {
+  set(target, key, value) {
+    // console.log('target', target)
+    // console.log('key', key)
+    // console.log('value', value)
+
+    if (key === 'message') {
+      target.longMessage = value + 'World!'
+    }
+    
+    target.message = value;
+  }
+}
+
+const proxy = new Proxy(data, handler)
+
+proxy.message = "Hello!!!"
+
+console.log('longMessage', proxy.longMessage)
