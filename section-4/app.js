aa58bb1076f9aee918aa290737d2b0c0ff957f9b
AlexBeje <alex.bejenaru93@gmail.com>
2022-09-13 16:49:28 +0200

    👌 IMPROVE: Added the actions log feature.

--- section-4/app.js
+++ section-4/app.js
@@ -19,6 +19,7 @@ Vue.createApp({
       },
       playerSurrendered: false,
       winnerTextColor: "black",
+      logMessages: [],
     };
   },
   computed: {
@@ -31,15 +32,18 @@ Vue.createApp({
     },
     winner() {
       if (this.gameOver) {
-        if (this.monsterHealth === this.playerHealth && !this.playerSurrendered) {
-          this.winnerTextColor = "orange";
+        if (
+          this.monsterHealth === this.playerHealth &&
+          !this.playerSurrendered
+        ) {
+          this.winnerTextColor = "#88005b";
           return "It's a Draw!";
         }
         if (this.monsterHealth > this.playerHealth || this.playerSurrendered) {
-          this.winnerTextColor = "red";
+          this.winnerTextColor = "#da8d00";
           return "The Monster won!";
         } else {
-          this.winnerTextColor = "green";
+          this.winnerTextColor = "#7700ff";
           return "You won!";
         }
       }
@@ -66,6 +70,7 @@ Vue.createApp({
       }
       this.currentRound++;
       const attackValue = this.getRandomValue(5, 12);
+      this.addLogMessage("player", "attack", attackValue);
       this.monsterHealth = this.getHealthByAttackValue(
         attackValue,
         this.monsterHealth
@@ -76,6 +81,7 @@ Vue.createApp({
     },
     attackPlayer() {
       const attackValue = this.getRandomValue(8, 15);
+      this.addLogMessage("monster", "attack", attackValue);
       this.playerHealth = this.getHealthByAttackValue(
         attackValue,
         this.playerHealth
@@ -103,14 +109,15 @@ Vue.createApp({
         this.abilities.specialAttack.used = true;
         this.abilities.specialAttack.roundsUntilAbilityCanBeUsed =
           this.abilities.specialAttack.coldown;
+        const attackValue = this.getRandomValue(10, 25);
+        this.addLogMessage("player", "specialAttack", attackValue);
+        this.monsterHealth = this.getHealthByAttackValue(
+          attackValue,
+          this.monsterHealth
+        );
+        this.updateAbilityColdown(this.abilities.heal);
+        this.attackPlayer();
       }
-      const attackValue = this.getRandomValue(10, 25);
-      this.monsterHealth = this.getHealthByAttackValue(
-        attackValue,
-        this.monsterHealth
-      );
-      this.updateAbilityColdown(this.abilities.heal);
-      this.attackPlayer();
     },
     heal() {
       if (this.gameOver) {
@@ -120,14 +127,15 @@ Vue.createApp({
         this.abilities.heal.used = true;
         this.abilities.heal.roundsUntilAbilityCanBeUsed =
           this.abilities.heal.coldown;
+        const healValue = this.getRandomValue(8, 20);
+        this.addLogMessage("player", "heal", healValue);
+        this.playerHealth = this.getHealthByHealValue(
+          healValue,
+          this.playerHealth
+        );
+        this.updateAbilityColdown(this.abilities.specialAttack);
+        this.attackPlayer();
       }
-      const healValue = this.getRandomValue(8, 20);
-      this.playerHealth = this.getHealthByHealValue(
-        healValue,
-        this.playerHealth
-      );
-      this.updateAbilityColdown(this.abilities.specialAttack);
-      this.attackPlayer();
     },
     surrender() {
       if (this.gameOver) {
@@ -151,6 +159,30 @@ Vue.createApp({
         },
       };
       this.playerSurrendered = false;
+      this.logMessages = [];
+    },
+    addLogMessage(who, what, value) {
+      // add something at the beginning of the array
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
+    actionByPlayer(logMessage) {
+      return logMessage.actionBy === "player";
+    },
+    actionByMonster(logMessage) {
+      return logMessage.actionBy === "monster";
+    },
+    healActionType(logMessage) {
+      return logMessage.actionType === "heal";
+    },
+    attackActionType(logMessage) {
+      return logMessage.actionType === "attack";
+    },
+    specialAttackActionType(logMessage) {
+      return logMessage.actionType === "specialAttack";
     },
   },
 }).mount("#game");
