8751f6d306f2146cae82943e53993c19e6775c80
AlexBeje <alex.bejenaru93@gmail.com>
2022-09-15 08:17:09 +0200

    📦 NEW: Added the new friend-contact component.

---
+++ section-7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ showHideLabel }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "lopez@localhost.com",
+      },
+    };
+  },
+  computed: {
+    showHideLabel() {
+      return this.detailsAreVisible ? "Hide" : "Show";
+    },
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
