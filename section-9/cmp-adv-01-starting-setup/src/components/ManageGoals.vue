b7afd8a0345381d548dc0fccabf2a46563bbbae1
AlexBeje <alex.bejenaru93@gmail.com>
2022-09-16 13:36:35 +0200

    📦 NEW: Create a dialog component.

--- section-9/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ section-9/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,33 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text">
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <error-alert v-if="inputInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Please enter at least a few characters</p>
+    <button @click="confirmError">Confirm</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputInvalid = false;
+    },
+  },
+};
+</script>
