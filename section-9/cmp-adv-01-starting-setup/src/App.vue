4adcd2a1a634d6a81761c64b6530759b793dee36
AlexBeje <alex.bejenaru93@gmail.com>
2022-09-16 13:09:47 +0200

    📦 NEW: Slot props.

--- section-9/cmp-adv-01-starting-setup/src/App.vue
+++ section-9/cmp-adv-01-starting-setup/src/App.vue
@@ -7,18 +7,28 @@
     :info-text="activeUser.description"
     :role="activeUser.role"
   />
+  <!-- Only works if just a slot is available (default slot) -->
+  <course-goals #default="slotProps">
+    <!-- <template #default="slotProps">
+      <h2 @click="test(slotProps)">{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+    </template> -->
+    <h2 @click="test(slotProps)">{{ slotProps.item }}</h2>
+    <p>{{ slotProps.anotherProp }}</p>
+  </course-goals>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   // Local components
   // components: { 'the-header': TheHeader },
   // components: { TheHeader: TheHeader },
-  components: { TheHeader, BadgeList, UserInfo },
+  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
   data() {
     return {
       activeUser: {
@@ -27,6 +37,11 @@ export default {
         role: 'admin',
       },
     };
+  },
+  methods: {
+    test(item) {
+      console.log('👩‍👩‍👦‍👦', item);
+    },
   },
 };
 </script>
