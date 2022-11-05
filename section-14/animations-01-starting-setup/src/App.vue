e93878cb4967b819c1e3433d51f7433a9ae4bead
AlexBeje <alex.bejenaru93@gmail.com>
2022-11-05 08:43:30 +0100

    📦 NEW: Added transition and transform units.

--- section-14/animations-01-starting-setup/src/App.vue
+++ section-14/animations-01-starting-setup/src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
   <div class="container">
-    <button @click="showDialog">Show Dialog</button>
+    <button @click="handler(arg, event)">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false
+    };
   },
   methods: {
     showDialog() {
@@ -24,6 +27,9 @@ export default {
     hideDialog() {
       this.dialogIsVisible = false;
     },
+    animateBlock() {
+      this.animatedBlock = true;
+    }
   },
 };
 </script>
@@ -57,6 +63,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +75,8 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  transform: translateX(-150px);
 }
 </style>
